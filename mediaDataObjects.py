52010e652cc9a856743c8303846d0b64f3b70627
markpippins <mpippins@gmail.com>
2016-07-29 13:03:37 -0400

    minor changes, less pythonic, easier to look at

--- mediaDataObjects.py
+++ mediaDataObjects.py
@@ -4,9 +4,21 @@ class MediaObject:
     def __init__(self):
         self.data = None
         self.file_size = 0
-        self.ext = self.file_name = self.folder_name = self.location = u''
-        self.compilation = self.expunged = self.extended_mix = self.ignore = self.incomplete = self.live = False
-        self.new = self.noscan = self.random = self.recent_download = self.unsorted = False
+        self.ext = u''
+        self.file_name = u''
+        self.folder_name = u''
+        self.location = u''
+        self.compilation = False
+        self.expunged = False
+        self.extended_mix = False
+        self.ignore = False
+        self.incomplete = False
+        self.live = False
+        self.new = False 
+        self.noscan = False
+        self.random = False
+        self.recent_download = False
+        self.unsorted = False
 
 class ScanCriteria:
     def __init__(self):
