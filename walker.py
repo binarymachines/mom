79b15b33c8391d8fa1a259fc963f5a6f326cde1e
markpippins <mpippins@gmail.com>
2016-08-11 00:50:50 -0400

    refactored MediaManager as subclass of MediaLibraryWalker.

--- walker.py
+++ walker.py
@@ -14,43 +14,45 @@ class Walker(object):
         self.debug = False
 
     def after_handle_root(self, root):
-        if self.debug: print 'finished with root directory: %s' % (root)
+        # if self.debug: print 'finished with root directory: %s' % (root)
         self.current_root = root
 
     def after_handle_file(self, filename):
-        if self.debug: print 'finished with filename: %s' % (filename)
+        # if self.debug: print 'finished with filename: %s' % (filename)
         self.current_filename = filename
 
     def after_handle_dir(self, directory):
-        if self.debug: print 'finished with directory: %s' % (directory)
+        # if self.debug: print 'finished with directory: %s' % (directory)
+        pass
 
     def before_handle_root(self, root):
-        if self.debug: print 'starting with root directory: %s' % (root)
+        # if self.debug: print 'starting with root directory: %s' % (root)
         self.current_root = root
 
     def before_handle_file(self, filename):
-        if self.debug: print 'starting with filename: %s' % (filename)
+        # if self.debug: print 'starting with filename: %s' % (filename)
         self.current_filename = filename
 
     def before_handle_dir(self, directory):
-        if self.debug: print 'starting with directory: %s' % (directory)
+        # if self.debug: print 'starting with directory: %s' % (directory)
+        pass
 
     def handle_root(self, root):
-        if self.debug: print 'current root directory: %s' % (root)
+        # if self.debug: print 'current root directory: %s' % (root)
         self.current_root = root
 
     def handle_root_error(self, error):
         pass
 
     def handle_file(self, filename):
-        if self.debug: print 'current filename: %s' % (filename)
+        # if self.debug: print 'current filename: %s' % (filename)
         self.current_filename = filename
 
     def handle_file_error(self, error):
         pass
 
     def handle_dir(self, directory):
-        if self.debug: print 'current directory: %s' % (directory)
+        # if self.debug: print 'current directory: %s' % (directory)
         self.current_dir = directory
 
     def handle_dir_error(self, error):
