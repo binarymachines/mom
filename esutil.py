9881035965352f44ef31dbf470007027b6c2ccf9
markpippins <mpippins@gmail.com>
2016-08-15 17:47:38 -0400

    post-refactor check-in

--- esutil.py
+++ esutil.py
@@ -42,28 +42,19 @@ def connect(hostname, portnum):
 
 def delete_docs_for_path(es, indexname, doctype, path):
 
-    rows = mySQL4es.retrieve_like_values('elasticsearch_doc', ['index_name', 'document_type', 'absolute_path', 'active_flag', 'id'], [indexname, doctype, path, True])
+    rows = mySQL4es.retrieve_like_values('es_document', ['index_name', 'document_type', 'absolute_path', 'active_flag', 'id'], [indexname, doctype, path, True])
     for r in rows:
         esid = r[4]
         res = es.delete(index=indexname,doc_type=doctype,id=esid)
         if res['_shards']['successful'] == 1:
-            update_values('elasticsearch_doc', 'active_flag', False, ['id'], [esid])
+            update_values('es_document', 'active_flag', False, ['id'], [esid])
 
 def find_docs_missing_field(es, index_name, document_type, field):
     query = { "query" : { "bool" : { "must_not" : { "exists" : { "field" : field }}}}}
     res = es.search(index=index_name, doc_type=document_type, body=query,size=1000)
     return res
 
-# def transform_docs():
-#     rows = retrieve_values('elasticsearch_doc', ['index_name', 'id', 'doc_type', 'absolute_path'], [])
-#     for r in rows:
-#         print r
-#         aws_insert_values(con, 'elasticsearch_doc', ['id', 'index_name', 'doc_type', 'absolute_path'], [r[0], r[1], r[2], r[3]])
-#
-#     con.commit()
-    # con.close()
-
-def main():
+def transform_docs():
     es = connect(constants.ES_HOST, constants.ES_PORT)
 
     cycle = True
@@ -84,6 +75,8 @@ def main():
 
     sys.exit(1)
 
+def main():
+    pass
 
 if __name__ == '__main__':
     main()
---
+++ operation.py
@@ -0,0 +1,49 @@
+#! /usr/bin/python
+
+import os, sys, time
+from elasticsearch import Elasticsearch
+import data, mySQL4es
+
+# def record_exists(self, asset):
+#     rows = mySQL4es.retrieve_values(asset.document_type, ['absolute_path'], [asset.absolute_path])
+#     if len(rows) == 0:
+#         return False
+#     else: return True
+#
+# def insert_record(self, asset):
+#     mySQL4es.insert_values(asset.document_type, ['absolute_path', 'latest_operation'],
+#         [asset.absolute_path, 'record_inserted'])
+#
+# def update_record(self, asset, update):
+#     mySQL4es.insert_values(asset.document_type, ['absolute_path', 'latest_operation'],
+#         [asset.absolute_path, update])
+
+def record(self, es, asset, operator, operation):
+    try:
+        if asset is not None and operation is not None:
+            if self.debug: print("recording operation: " + operation + ", " + asset.esid + ", " + folder.absolute_path)
+            dt = datetime.datetime.now().isoformat()
+            # update es with operation
+            res = es.update(index=self.index_name, doc_type=asset.document_type, id=asset.esid, body={"doc": {"latest_operation": operation }})
+
+            if asset.latest_operation_start_time == None:
+                asset.latest_operation = operation
+                asset.latest_operation_start_time = dt
+                # insert operation into MySQL
+                mySQL4es.insert_values('op_record', ['pid', 'operator_name', 'operation_name', 'target_esid', 'start_time'],
+                    [str(self.pid), operator, operation, asset.esid, dt])
+            else:
+                # update operation status in MySQL
+                mySQL4es.update_values('op_record', ['end_time'], [dt], ['operator_name', 'operation_name', 'target_esid'],
+                    [operator, operation, asset.esid])
+                asset.latest_operation = None
+                asset.latest_operation_start_time = None
+                # mySQL4es.update_values('op_record', ['end_time'], [dt], ['operator_name', 'operation_name', 'target_esid', 'start_time'],
+                #     [self.__class__.__name__, operation, folder.esid, folder.latest_operation_start_time])
+
+    except ConnectionError, err:
+        print ': '.join([err.__class__.__name__, err.message])
+        # if self.debug:
+        traceback.print_exc(file=sys.stdout)
+        print '\nConnection lost, please verify network connectivity and restart.'
+        sys.exit(1)
