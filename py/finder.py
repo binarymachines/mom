aba75cbcc3c476244a74e2bfa17f7046420dc7cc
markpippins <mpippins@gmail.com>
2016-08-30 14:04:37 -0400

    bug fixes, minor format changes

--- py/finder.py
+++ py/finder.py
@@ -3,7 +3,7 @@
 import os, sys, pprint, json
 
 import config, constants, mySQL4es, esutil
-from data import Asset
+from data import MediaFile, MediaFolder
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -15,11 +15,13 @@ def get_folders(path):
 
     return mySQL4es.run_query(q)
 
-def generate_match_doc(source_path, always_generate= False, outputfile=None):
-    
+def generate_match_doc(source_path, always_generate= False, outputfile=None, append_existing=False):
+    all_data = { 'match_results': source_path}
+    all_data['source_folders'] = []
+
     folders = get_folders(source_path)
     for folder in folders:
-        mediaFolder = Asset()
+        mediaFolder = MediaFolder()
         mediaFolder.esid = folder[0]
         mediaFolder.absolute_path = folder[1]
         
@@ -57,10 +59,13 @@ def generate_match_doc(source_path, always_generate= False, outputfile=None):
                 match_score = match[1]
                 match_esid = match[2]
                 match_abs_path = match[3]
+                match_comp_result = match[4]
                 match_filename = match_abs_path.split('/')[-1]
                 match_parent_dir = os.path.abspath(os.path.join(match_abs_path, os.pardir))
+                suggestion = 'DELETE' if match_comp_result in ['=', '>'] else 'PROMOTE'
+                match_data = { '_matcher': matcher_name, '_match_score': match_score, '_match_esid': match_esid, '_match_filename': match_filename, '_suggestion': suggestion }
+                media_data['_suggestion'] = suggestion = 'DELETE' if match_comp_result in ['<'] else 'KEEP'
 
-                match_data = { '_matcher': matcher_name, '_match_score': match_score, '_match_esid': match_esid, '_match_filename': match_filename }
                 get_media_meta_data(match_esid, match_data)
                 if not match_parent_dir in parent_data:
                     parent_data[match_parent_dir] = {'_match_folder': match_parent_dir }
@@ -75,13 +80,14 @@ def generate_match_doc(source_path, always_generate= False, outputfile=None):
             match_files.append(file_data)
 
         folder_data["matches"] = match_files
-
         if matches_exist or always_generate: 
-            handle_results(folder_data, outputfile)
+            all_data['source_folders'].append(folder_data)
+    
+    handle_results(all_data, outputfile, append_existing)
             
 def get_matches(esid):
     
-    q = """SELECT DISTINCT m.matcher_name, m.match_score, m.match_doc_id, es.absolute_path 
+    q = """SELECT DISTINCT m.matcher_name, m.match_score, m.match_doc_id, es.absolute_path, m.comparison_result 
                 FROM matched m, es_document es 
             WHERE es.index_name = '%s' and es.id = m.match_doc_id and m.media_doc_id = '%s'
             ORDER BY m.matcher_name, es.absolute_path""" % (constants.ES_INDEX_NAME, esid)
@@ -97,17 +103,19 @@ def get_media_files(path):
                 
     return mySQL4es.run_query(q)
 
-def handle_results(results, outputfile):
+def handle_results(results, outputfile, append_existing):
     if outputfile is None: 
         pp.pprint(folder_data)
     else:
-        with open(outputfile, 'wt') as out:
+        print 'writing output..'
+        write_method = 'at' if append_existing else 'wt'
+        with open(outputfile, write_method) as out:
             pprint.pprint(results, stream=out)
             out.close()
-
+        
 # TODO:make this return a boolean based on whether or not the doc is actually available and gracefully deal with the case where it is not
 def get_media_meta_data(esid, media_data):
-    mediaFile = Asset()
+    mediaFile = MediaFile()
     mediaFile.esid = esid
     # mediaFile.absolute_path = media[1]
     mediaFile.document_type = 'media_file'
@@ -128,7 +136,9 @@ def get_media_meta_data(esid, media_data):
 
 def main():
     config.configure()
-    generate_match_doc('front 242/geography', False, 'geography.json') 
+    folder = 'geography'
+    outputfile = '.'.join([folder.split('/')[-1], 'json'])
+    generate_match_doc(folder, False, outputfile, False) 
 
 # main
 if __name__ == '__main__':
