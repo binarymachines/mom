69c254067790b7d7938cffb5772b553bbf3682f0
markpippins <mpippins@gmail.com>
2016-08-31 16:30:41 -0400

    using constants.DEBUG values

--- py/mySQL4es.py
+++ py/mySQL4es.py
@@ -5,39 +5,12 @@ import MySQLdb as mdb
 import constants
 from data import AssetException
 
-DEBUG = True
-
 def quote_if_string(value):
     if isinstance(value, basestring):
         return '"%s"' % value
     if isinstance(value, unicode):
         return u'"' + value + u'"'
     return value
-
-def aws_insert_values(con, table_name, field_names, field_values):
-
-    # con = None
-    try:
-        formatted_values = [quote_if_string(value) for value in field_values]
-
-        query = 'INSERT INTO %s(%s) VALUES(%s)' % (table_name, ','.join(field_names), ','.join(formatted_values))
-
-        if DEBUG:
-            print '\n\t' + query.replace(',', ',\n\t\t').replace(' values ', '\n\t   values\n\t\t').replace('(', ' (\n\t\t').replace(')', '\n\t\t)') + '\n'
-
-        # con = mdb.connect(AWS_HOST, AWS_USER, AWS_PASS, SCHEMA)
-        cur = con.cursor()
-        cur.execute(query)
-        # con.commit()
-
-    except mdb.Error, e:
-
-        message = "Error %d: %s" % (e.args[0], e.args[1])
-        # raise Exception(message)
-        print message
-    # finally:
-    #     if con:
-    #         con.close()
 
 def insert_values(table_name, field_names, field_values):
 
@@ -48,7 +21,7 @@ def insert_values(table_name, field_names, field_values):
 
         query = 'INSERT INTO %s(%s) VALUES(%s)' % (table_name, ','.join(field_names), ','.join(formatted_values))
 
-        if DEBUG:
+        if constants.SQL_DEBUG:
             print '\n\t' + query.replace(',', ',\n\t\t').replace(' values ', '\n\t   values\n\t\t').replace('(', ' (\n\t\t').replace(')', '\n\t\t)') + '\n'
 
         con = mdb.connect(constants.MYSQL_HOST, constants.MYSQL_USER, constants.MYSQL_PASS, constants.MYSQL_SCHEMA)
@@ -61,13 +34,6 @@ def insert_values(table_name, field_names, field_values):
         print message
         raise e
 
-    # except UnicodeDecodeError, e:
-    #     raise e
-    #
-    # except Exception, e:
-    #     # raise Exception(message)
-    #     print e.message
-    #     raise e
     finally:
         if con:
             con.close()
@@ -101,7 +67,7 @@ def retrieve_values(table_name, field_names, field_values):
                     query += ' AND '
                 else: break
 
-        if DEBUG:
+        if constants.SQL_DEBUG:
             print '\n\t' + query.replace('WHERE', '\n\t      WHERE').replace('AND', '\n\t\tAND').replace('FROM', '\n\t       FROM')
 
         con = mdb.connect(constants.MYSQL_HOST, constants.MYSQL_USER, constants.MYSQL_PASS, constants.MYSQL_SCHEMA)
@@ -109,7 +75,7 @@ def retrieve_values(table_name, field_names, field_values):
         cur.execute(query)
         rows = cur.fetchall()
 
-        if DEBUG:
+        if constants.SQL_DEBUG:
             if len(rows) > 0: print('\nreturning rows:\n')
             for row in rows:
                 print row
@@ -147,7 +113,7 @@ def retrieve_like_values(table_name, field_names, field_values):
                 query += ' AND '
             else: break
 
-        if DEBUG:
+        if constants.SQL_DEBUG:
             print '\n\t' + query.replace('WHERE', '\n\t      WHERE').replace('AND', '\n\t\tAND').replace('FROM', '\n\t       FROM')
 
         con = mdb.connect(constants.MYSQL_HOST, constants.MYSQL_USER, constants.MYSQL_PASS, constants.MYSQL_SCHEMA)
@@ -155,7 +121,7 @@ def retrieve_like_values(table_name, field_names, field_values):
         cur.execute(query)
         rows = cur.fetchall()
 
-        if DEBUG:
+        if constants.SQL_DEBUG:
             if len(rows) > 0: print('\nreturning rows:\n')
             for row in rows:
                 print row
@@ -172,7 +138,7 @@ def retrieve_like_values(table_name, field_names, field_values):
 
 def run_query(query):
 
-    if DEBUG: print query
+    if constants.SQL_DEBUG: print query
     con = None
     rows = []
 
@@ -183,7 +149,7 @@ def run_query(query):
         cur.execute(query)
         rows = cur.fetchall()
 
-        if DEBUG:
+        if constants.SQL_DEBUG:
             if len(rows) > 0: print('\nreturning rows:\n')
             for row in rows:
                 print row
@@ -210,12 +176,6 @@ def execute_query(query):
         con = mdb.connect(constants.MYSQL_HOST, constants.MYSQL_USER, constants.MYSQL_PASS, constants.MYSQL_SCHEMA)
         cur = con.cursor()
         cur.execute(query)
-        # rows = cur.fetchall()
-        #
-        # if DEBUG:
-        #     if len(rows) > 0: print('\nreturning rows:\n')
-        #     for row in rows:
-        #         print row
         con.commit()
     except mdb.Error, e:
 
@@ -252,7 +212,7 @@ def update_values(table_name, update_field_names, update_field_values, where_fie
             query += ' AND '
         else: break
 
-    if DEBUG:
+    if constants.SQL_DEBUG:
         print '\n\t' + query.replace('WHERE', '\n\t WHERE').replace(', ', ',\n\t       ').replace('SET', '\n\t   SET').replace('AND', '\n\t   AND')
 
     try:
@@ -292,10 +252,10 @@ def truncate(table_name):
 
 def ensure_exists(esid, path, indexname, documenttype):
     try:
-        if DEBUG: print("checking for row for: "+ path)
+        if constants.SQL_DEBUG: print("checking for row for: "+ path)
         rows = retrieve_values('es_document', ['absolute_path', 'index_name'], [path, indexname])
         if len(rows) ==0:
-            if DEBUG: print('Updating local MySQL...')
+            if constants.SQL_DEBUG: print('Updating local MySQL...')
             insert_esid(indexname, documenttype, esid, path)
     except mdb.Error, e:
         print "Error %d: %s" % (e.args[0], e.args[1])
