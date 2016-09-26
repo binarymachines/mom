10cd3968bbfd7f7212d21d24644950f49a7e0b23
mpippins <markpippins@hotmail.com>
2016-09-25 22:40:32 -0400

    bug fix

--- py/sql/cache_retrieve_docs.sql
+++ py/sql/cache_retrieve_docs.sql
@@ -5,5 +5,5 @@ SELECT absolute_path, id
   FROM es_document
  WHERE index_name = '%s'
    AND doc_type = '%s'
-   AND absolute_path LIKE '%s'
+   AND absolute_path LIKE '%s*'
  ORDER BY absolute_path
