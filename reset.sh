69ecae9967848029a1a99142b7ea03970d78bc25
markpippins <markpippins@hotmail.com>
2016-10-29 21:02:39 -0400

    snapshot

--- reset.sh
+++ reset.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
-rm $m2/py/*.pyc
-rm $m2/logs/*.log
 clear
-~/dev/_virtual_env/mom/bin/python2.7 $m2/py/launch.py --reset --exit
+~/dev/_virtual_env/mom/bin/python2.7 $m2/python/mildred/launch.py --reset --exit
--- resolver.sh
+++ resolver.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 pushd  /home/$USER/dev/m2
-clear
 ./js/electron/node_modules/.bin/electron js/electron/
 popd
