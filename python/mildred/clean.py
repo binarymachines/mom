29c1ba796481412797227428e6cf71eba8d43ade
markpippins <markpippins@hotmail.com>
2016-11-02 16:38:45 -0400

    snapshot

--- python/mildred/clean.py
+++ python/mildred/clean.py
@@ -1,10 +1,16 @@
-import search
-
-"""housekeeping mode"""
+import search, sql, library
+from const import CLEAN
+from errors import MultipleDocsException
 
 def clean(context):
-    pass
 
-def find_and_remove_dupes():
-    pass
-	
+    while self.context.has_next(CLEAN, True):
+        ops.check_status()
+        path = self.context.get_next(CLEAN, True)
+        rows = sql.retrieve_like_values('document', ['absolute_path', 'document_type', '_hex_id'], [path]) 
+        for row in rows: 
+            try:
+                search.unique_doc_exists(row[1], '_hex_id', row[2], except_on_multiples=True):
+            except MultipleDocsException, err:
+                library.handle_asset_exception(err, row[0])
+
