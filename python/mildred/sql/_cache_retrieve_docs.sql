d2544f1934532caf745d99af5eeff51f77b197b0
markpippins <markpippins@hotmail.com>
2016-11-06 02:09:04 -0500

    snapshot

--- python/mildred/sql/_cache_retrieve_docs.sql
+++
@@ -1,9 +0,0 @@
--- retrieve_docs: get documents for supplied path
--- params: index_name, doc_type, absolute_path
---
-SELECT absolute_path, id
-  FROM document
- WHERE index_name = '%s'
-   AND doc_type = '%s'
-   AND absolute_path LIKE '%s*'
- ORDER BY absolute_path
--- python/mildred/sql/report_get_media_files_reverse.sql
+++
@@ -1,8 +0,0 @@
--- get_matches: get reverse matched documents for a supplied asset path
--- params: index_name, absolute_path
---
-SELECT es.id, es.absolute_path FROM document es
- WHERE index_name = '%s'
-   and es.absolute_path LIKE '%s*'
-   and es.id IN (SELECT match_doc_id FROM matched)
- ORDER BY es.absolute_path
