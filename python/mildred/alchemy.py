182fc362075c2ed5c803fb5955c8ee6e762d4139
markpippins <markpippins@hotmail.com>
2016-11-07 12:06:23 -0500

    alchemy

--- python/mildred/alchemy.py
+++ python/mildred/alchemy.py
@@ -326,7 +326,8 @@ class SQLModeStateRecord(Base):
 #   `effective_dt` datetime DEFAULT NULL,
 #   `expiration_dt` datetime DEFAULT NULL,
 #   PRIMARY KEY (`id`)
-# )
+# )ll
+
 
 
 class SQLModeStateTransitionErrorRecord(Base):
