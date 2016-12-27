184ad1be2e3c895bc62e186e0973950a0c3b99f3
markpippins <markpippins@hotmail.com>
2016-12-27 04:53:57 -0500

    snapshot

---
+++ bin/db-bak.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+pushd $M2
+
+clear
+
+echo '----------------------------'
+echo 'Backing up Mildred Databases'
+echo '----------------------------'
+echo
+
+echo "removing existing backups..."
+rm bak/sql/*.sql
+rm bak/sql/dump/*.sql
+rm setup/sql/*.sql
+
+echo "copying lookup tables..."
+
+./bin/copy-table-data.sh mildred directory
+./bin/copy-table-data.sh mildred directory_amelioration
+./bin/copy-table-data.sh mildred directory_attribute
+./bin/copy-table-data.sh mildred directory_constant
+
+./bin/copy-table-data.sh mildred document_category
+./bin/copy-table-data.sh mildred document_metadata
+
+./bin/copy-table-data.sh mildred file_format
+./bin/copy-table-data.sh mildred file_type
+
+./bin/copy-table-data.sh mildred match_discount
+./bin/copy-table-data.sh mildred match_weight
+./bin/copy-table-data.sh mildred matcher
+./bin/copy-table-data.sh mildred matcher_field
+
+./bin/copy-table-data.sh mildred_admin org
+./bin/copy-table-data.sh mildred_admin member
+
+./bin/copy-table-data.sh mildred_introspection mode
+./bin/copy-table-data.sh mildred_introspection state
+./bin/copy-table-data.sh mildred_introspection mode_state_default
+./bin/copy-table-data.sh mildred_introspection mode_state_default_param
+#./bin/copy-table-data.sh mildred_introspection error
+#./bin/copy-table-data.sh mildred_introspection error_attribute
+
+./bin/copy-table-data.sh media artist_alias
+./bin/copy-table-data.sh media artist_amelioration
+
+echo "adding lookup tables to git."
+git add bak/sql/*.sql
+
+mysqldump --routines scratch > bak/sql/scratch.sql
+
+echo "copying setup tables..."
+./bin/copy-table-ddl.sh mildred setup/sql/setup-mildred.sql
+./bin/copy-table-ddl.sh mildred_admin setup/sql/setup-mildred_admin.sql
+./bin/copy-table-ddl.sh mildred_action setup/sql/setup-mildred_action.sql
+./bin/copy-table-ddl.sh mildred_introspection setup/sql/setup-mildred_introspection.sql
+./bin/copy-table-ddl.sh media setup/sql/setup-media.sql
+./bin/copy-table-ddl.sh scratch setup/sql/backup-media-no-data.sql
+
+echo "adding setup tables to git."
+git add setup/sql/*.sql
+
+echo "dumping schemas..."
+
+./bin/backup-schema.sh mildred
+./bin/backup-schema.sh mildred_admin
+./bin/backup-schema.sh mildred_action
+./bin/backup-schema.sh mildred_introspection
+./bin/backup-schema.sh media
+./bin/backup-schema.sh scratch
+
+echo '----------------------------'
+echo 'Database backup is complete!'
+echo '----------------------------'
+echo
+
+git status
+git commit -m 'db snapshot'
+git push
+
+popd
