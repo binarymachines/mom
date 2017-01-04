a32236227bcf96ea68e23a528e1625896640f2f9
Mark Pippins <markpippins@hotmail.com>
2017-01-03 20:28:40 -0500

    snapshot

--- db/setup/setup.sql
+++
@@ -1,7 +0,0 @@
-create schema if not exists `media`;
-create schema if not exists `mildred`;
-create schema if not exists `mildred_action`;
-create schema if not exists `mildred_admin`;
-create schema if not exists `mildred_introspection`;
-create schema if not exists `mildred_error`;
-create schema if not exists `scratch`;
