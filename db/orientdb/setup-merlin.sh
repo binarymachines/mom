239f5c9e1d7a172fffd05431c81c5b64f3873281
Mark Pippins <markpippins@hotmail.com>
2017-01-11 15:37:58 -0500

    snapshot

--- db/orientdb/setup-merlin.sh
+++ db/orientdb/setup-merlin.sh
@@ -5,6 +5,7 @@ pushd $ORIENTDB_HOME/bin
 
 ./oetl.sh $M2/db/orientdb/etl-import-dispatch.json
 ./oetl.sh $M2/db/orientdb/etl-import-reason.json
+./oetl.sh $M2/db/orientdb/etl-import-reason-param.json
 ./oetl.sh $M2/db/orientdb/etl-import-action.json
 ./oetl.sh $M2/db/orientdb/etl-import-action-reason.json
 
