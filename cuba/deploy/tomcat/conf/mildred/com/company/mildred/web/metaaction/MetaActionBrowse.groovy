1d49008457e3ba01818f864cd44fb0fd3821b5b3
Mark Pippins <markpippins@hotmail.com>
2018-04-13 10:48:37 -0400

    spapshot

--- cuba/deploy/tomcat/conf/mildred/com/company/mildred/web/metaaction/MetaActionBrowse.groovy
+++
@@ -1,6 +0,0 @@
-package com.company.mildred.web.metaaction
-
-import com.haulmont.cuba.gui.components.AbstractLookup
-
-class MetaActionBrowse extends AbstractLookup {
-}
--- cuba/deploy/tomcat/conf/mildred/com/company/mildred/web/metareason/MetaReasonBrowse.groovy
+++
@@ -1,6 +0,0 @@
-package com.company.mildred.web.metareason
-
-import com.haulmont.cuba.gui.components.AbstractLookup
-
-class MetaReasonBrowse extends AbstractLookup {
-}
