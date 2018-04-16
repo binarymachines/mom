ac423c66efcaa8cca1e9f2ff022a0aec0407751e
markpippins <mpippins@gmail.com>
2018-04-15 23:36:54 -0400

    snapshot

--- cuba/deploy/tomcat/conf/mildred/com/company/mildred/web/switchrule/SwitchRuleEdit.java
+++
@@ -1,7 +0,0 @@
-package com.company.mildred.web.switchrule;
-
-import com.haulmont.cuba.gui.components.AbstractEditor;
-import com.company.mildred.entity.SwitchRule;
-
-public class SwitchRuleEdit extends AbstractEditor<SwitchRule> {
-}
