8f234133f6fc857dc42ee06a5cf71168546ec6a6
Mark Pippins <markpippins@hotmail.com>
2018-04-17 16:20:37 -0400

    snapshot

---
+++ cuba/deploy/tomcat/conf/mildred/com/angrysurfer/mildred/web/media/document/DocumentEdit.java
@@ -0,0 +1,7 @@
+package com.angrysurfer.mildred.web.media.document;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.angrysurfer.mildred.entity.media.Document;
+
+public class DocumentEdit extends AbstractEditor<Document> {
+}
