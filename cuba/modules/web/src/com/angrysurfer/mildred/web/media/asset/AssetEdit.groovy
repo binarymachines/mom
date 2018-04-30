280d2c941569e431acea985117942dd85bef825b
Mark Pippins <markpippins@hotmail.com>
2018-04-30 10:33:51 -0400

    bugfix

--- cuba/modules/web/src/com/angrysurfer/mildred/web/media/asset/AssetEdit.groovy
+++ cuba/modules/web/src/com/angrysurfer/mildred/web/media/asset/AssetEdit.groovy
@@ -20,7 +20,7 @@ class AssetEdit extends AbstractEditor<Asset> {
     @Override
     protected void postInit() {
         super.postInit();
-        if (getItem()) {
+        if (getItem().assetType) {
             String doc = "http://localhost:9200/${getItem().assetType}/_search?pretty;q=_id:${getItem().id}".toURL().text
             getItem().setDocument(doc)
         }
